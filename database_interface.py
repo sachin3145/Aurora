fa971c4b2612edcf89462442db45978c9d65df07
kevin8376 <sachin3145@outlook.com>
2020-05-14 18:50:16 -0700

    second sql file

--- database_interface.py
+++ database_interface.py
@@ -1,10 +1,15 @@
 import mysql.connector
 
 
-def create_user(name, key):
+def create_user():
 
-    db = mysql.connector.connect(host='localhost', user='root', passwd = key)
+    db = mysql.connector.connect(host='localhost', user='root', passwd = 'openupuser')
     p = db.cursor()
-    sql_file = open('SQL/script001.sql')
+    sql_file = open('SQL/script002.sql')
+    print(sql_file.read())
     p.execute(sql_file.read())
+
+
+create_user()
+
 
