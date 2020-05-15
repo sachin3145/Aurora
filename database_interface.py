f359e44894fbbf8553014a54abec62de49cafa32
kevin8376 <sachin3145@outlook.com>
2020-05-14 18:42:32 -0700

    Created demo table structure

--- database_interface.py
+++ database_interface.py
@@ -6,5 +6,5 @@ def create_user(name, key):
     db = mysql.connector.connect(host='localhost', user='root', passwd = key)
     p = db.cursor()
     sql_file = open('SQL/script001.sql')
-    p.execute(sql_file.read().format(name))
+    p.execute(sql_file.read())
 
