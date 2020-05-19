31fee5ea8192a4cf454afa04de1125da951e1df1
kevin8376 <sachin3145@outlook.com>
2020-05-19 12:23:41 +0530

    updated datainterface

--- database_interface.py
+++ database_interface.py
@@ -1,6 +1,15 @@
 import mysql.connector
 
+key = input('Please Enter mysql root password : ')
 
-db = mysql.connector.connect(host='localhost', user='root', passwd = 'openupuser')
-p = db.cursor()
-p.execute(open('SQL/script002.sql').read())
+try:
+    db = mysql.connector.connect(host='localhost', user='root', passwd = key)
+    if db.is_connected(): print('Successfully connected')
+    del key
+    p = db.cursor()
+    p.execute(open('SQL/script002.sql').read())
+    p.close()
+except:
+    print('Connection Failed!')
+
+
