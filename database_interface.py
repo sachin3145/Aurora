bc567f4d10d22839f7d9a03dbdb1f40bf370d59f
kevin8376 <sachin3145@outlook.com>
2020-05-19 22:47:29 +0530

    Gameplay objects and effects 001

--- database_interface.py
+++ database_interface.py
@@ -3,13 +3,15 @@ import mysql.connector
 key = input('Please Enter mysql root password : ')
 
 try:
-    db = mysql.connector.connect(host='localhost', user='root', passwd = key)
-    if db.is_connected(): print('Successfully connected')
+    db = mysql.connector.connect(host='localhost', user='root', passwd=key)
     del key
+    if db.is_connected():
+        print('Successfully connected')
     p = db.cursor()
     p.execute(open('SQL/script002.sql').read())
     p.close()
+
 except:
     print('Connection Failed!')
-
-
+    if eval(input('Enter 0 to exit or 1 to troubleshoot : ')) == 1:
+        raise
