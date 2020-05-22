234cc4b6b9638c349b3a1e25252ca0acd89836c5
naraian-6432 <rahuln.61375@gmail.com>
2020-05-22 08:32:03 +0530

    new commit

--- python/main.py
+++ python/main.py
@@ -1,1 +1,25 @@
+# importing pygame library
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# create screen
+screen = pygame.display.set_mode((800, 600))
+
+# title and icon
+pygame.display.set_caption("The Helios Attack")
+icon = pygame.image.load('icon.png')
+pygame.display.set_icon(icon)
+
+# Game loop
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+#RGB- reg, green, blue
+    screen.fill((0, 0, 128))
+    pygame.display.update()
 
