2c39d6ee3e9d5432e3ab0bd22c133031ec6e4821
kevin8376 <sachin3145@outlook.com>
2020-05-29 23:04:41 +0530

    removed redundandat files

--- python/main.py
+++ python/main.py
@@ -1,7 +1,29 @@
-from .screen_dimensions import *
+import pygame
+from ctypes import windll
+
+
+user32 = windll.user32
+screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1) - 40
+screensize = screen_width, screen_height
+
+
+def sh(percentage):
+    return int(percentage*screen_height/100)
+
+
+def sw(percentage):
+    return int(percentage*screen_width/100)
+
+
+def pix_w(pix):
+    return pix*(100/screen_width)
+
+
+def pix_h(pix):
+    return pix*(100/screen_height-40)
+
 
 # initializing pygame
-import pygame
 pygame.init()
 
 # create screen
@@ -79,8 +101,6 @@ def menu():
 
         pygame.draw.rect(screen, (0, 255, 0), (619, 384, 128, 64))
         pygame.display.update()
-
-
 
 
 # Main loop
