4c1d72982af02449fb7cc08accf00099420d2e97
kevin8376 <sachin3145@outlook.com>
2020-05-31 00:42:55 +0530

    started segreagating program into seperate files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from ctypes import windll
+from Aurora.Packages.screen_dimensions import *
+
 # --------------------------------------------------------------------------------------------------------
 ''' 
 background music
@@ -12,29 +13,7 @@ pygame.init()
 mixer.music.load('background.wav')
 mixer.music.play()
 '''
-# --------------------------------------------------------------------------------------------------------
-user32 = windll.user32
-screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1) - 40
-screensize = screen_width, screen_height
-
-
-def sh(percentage):
-    return int(percentage*screen_height/100)
-
-
-def sw(percentage):
-    return int(percentage*screen_width/100)
-
-
-def pix_w(pix):
-    return pix*(100/screen_width)
-
-
-def pix_h(pix):
-    return pix*(100/screen_height)
-
-# -----------------------------------------------------------------------------------------------------------
-
+# ----------------------------------------------------------------------------------------------------------
 
 # initializing pygame
 pygame.init()
