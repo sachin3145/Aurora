cc5aed81ccd9d8a9caf45431ca3fa3b8396d80ed
kevin8376 <sachin3145@outlook.com>
2020-06-01 20:29:13 +0530

    removed redundant code

--- Packages/screen_dimensions.py
+++ Packages/screen_dimensions.py
@@ -20,31 +20,3 @@ def pix_w(pix):
 
 def pix_h(pix):
     return pix*(100/screen_height)
-
-
-"""
-from ctypes import windll
-
-
-user32 = windll.user32
-screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1) - 40
-screensize = screen_width, screen_height
-
-
-def sh(percentage):
-    return int(percentage*screen_height/100)
-
-
-def sw(percentage):
-    return int(percentage*screen_width/100)
-
-
-def pix_w(pix):
-    return pix*(100/screen_width)
-
-
-def pix_h(pix):
-    return pix*(100/screen_height)
-
-
-"""
