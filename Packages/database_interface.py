3716214dac7e16c579df240d64b3559bcd5f7be3
kevin8376 <sachin3145@outlook.com>
2020-06-19 17:54:29 +0530

    database interface raw model

--- Packages/database_interface.py
+++ Packages/database_interface.py
@@ -1,26 +1,28 @@
 import mysql.connector
 
 key = input('Please Enter mysql root password : ')
-db = mysql.connector.connect(host='localhost', user='root', passwd=key)
-
-if db.is_connected():
-    print('connected')
-
 
 try:
     db = mysql.connector.connect(host='localhost', user='root', passwd=key)
     del key
     if db.is_connected():
         print('Successfully connected')
-    p = db.cursor()
-    p.execute(open('../SQL/script002.sql').read())
-
-    def execute(code):
-        p.execute(code)
-
-    p.close()
 
 except:
     print('Connection Failed!')
     if eval(input('Enter 1 to troubleshoot or any other key to exit : ')) == 1:
         raise
+    exit()
+
+
+p = db.cursor()
+#    p.execute(open('../SQL/script002.sql').read())
+
+
+def execute(code):
+    p.execute(code)
+    return p.fetchall()
+
+
+print(execute('Select CURDATE();'))
+p.close()
