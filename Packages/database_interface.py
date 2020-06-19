ae5b8b0051c0ee59893a0585617641ddc20ac980
kevin8376 <sachin3145@outlook.com>
2020-06-19 11:07:14 +0530

    Database interface - 1

--- Packages/database_interface.py
+++ Packages/database_interface.py
@@ -6,7 +6,7 @@ db = mysql.connector.connect(host='localhost', user='root', passwd=key)
 if db.is_connected():
     print('connected')
 
-'''
+
 try:
     db = mysql.connector.connect(host='localhost', user='root', passwd=key)
     del key
@@ -14,10 +14,13 @@ try:
         print('Successfully connected')
     p = db.cursor()
     p.execute(open('../SQL/script002.sql').read())
+
+    def execute(code):
+        p.execute(code)
+
     p.close()
 
 except:
     print('Connection Failed!')
     if eval(input('Enter 1 to troubleshoot or any other key to exit : ')) == 1:
         raise
-'''
