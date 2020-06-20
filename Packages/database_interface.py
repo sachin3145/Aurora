cf9e6c241e79a7c15ef6b69723e5ee93dc8a31d6
kevin8376 <sachin3145@outlook.com>
2020-06-20 08:20:39 +0530

    database interface functions - 1

--- Packages/database_interface.py
+++ Packages/database_interface.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
-key = input('Please Enter mysql root password : ')
-
+#key = input('Please Enter mysql root password : ')
+key = 'openupuser'
 try:
     db = mysql.connector.connect(host='localhost', user='root', passwd=key)
     del key
@@ -15,13 +15,32 @@ except:
     exit()
 
 
-p = db.cursor()
-#    p.execute(open('../SQL/script002.sql').read())
+p = db.cursor(buffered=True)
+p.execute(open('../SQL/script001.sql').read(), multi=True)
 
 
-def execute_sql(code):
-    p.execute(code)
-    return p.fetchall()
+def execute_sql(query):
+    """can be used to execute sql query"""
+    p.execute(query)
+    if p.rowcount > 0:
+        return p.fetchall()
 
 
-p.close()
+def is_unlocked(player_id, name, a_type):
+    if a_type == 'spell':
+        a_type = 'player_spells'
+    elif a_type == 'troop':
+        a_type = 'player_troops'
+    else:
+        return
+    execute_sql('USE AURORA;')
+    data_set = execute_sql('SELECT {} FROM {} WHERE PLAYER_ID = {};'.format(name, a_type, player_id))
+    if data_set is not None and data_set[0][0] == 1:
+        return True
+    return False
+
+
+# print(is_unlocked('0123456789', 'RAY_OF_SICKNESS', 'spell')) -- TEST
+
+if __name__ == '__main__':
+    p.close()
