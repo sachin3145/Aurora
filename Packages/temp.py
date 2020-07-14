bcce78d072fde90e4a92ccfdfdb50596957d4aa2
kevin8376 <sachin3145@outlook.com>
2020-07-14 23:34:50 +0530

    documentation

--- Packages/temp.py
+++ Packages/temp.py
@@ -196,6 +196,7 @@ class TextInput(Text):
         self.is_selected = False
 
     def try_selecting(self, event):
+        """Checks if user has selected the input box."""
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
             if self.rect.collidepoint(x, y):
@@ -218,6 +219,7 @@ class TextInput(Text):
         self.take_input(event)
 
     def render(self):
+        """Display the text on screen"""
         pygame.draw.rect(screen, self.color, self.rect, 1)
         text_surface = self.font.render(self.text, True, (255, 255, 255))
         screen.blit(text_surface, (self.rect.x + 5, self.rect.y + 3))
